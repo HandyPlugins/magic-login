fa1c3e7e5ddc359292535d30efecb12cd23fcecc
mustafauysal <me@uysalmustafa.com>
2022-09-12 10:31:35 +0300

    fix notice

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -117,7 +117,7 @@ function shortcode_login_form( $shortcode_atts ) {
  * @return mixed
  */
 function maybe_shortcode_redirect( $redirect_url, $user ) {
-	if ( $_GET['redirect_to'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+	if ( isset( $_GET['redirect_to'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		$redirect_url = esc_url_raw( $_GET['redirect_to'] ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 	}
 
