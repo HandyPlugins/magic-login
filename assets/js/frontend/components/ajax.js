c0261fc4a21c56ef8c9c8854e87767c92e32e9af
mustafauysal <me@uysalmustafa.com>
2025-03-04 02:21:41 +0300

    add ajax support for code login

--- assets/js/frontend/components/ajax.js
+++ assets/js/frontend/components/ajax.js
@@ -33,6 +33,13 @@ window.magicLoginAjaxEnabled = true;
 					$form.hide();
 				}
 
+				if (response.data.code_form) {
+					$form.parent().replaceWith(response.data.code_form);
+					$('.magic-login-code-form-header').find('.info').html($(response.data.info).text()).show();
+					$('#magiclogincodeform').find('.magic-login-code-cancel').hide();
+					maybeInitializeSpamProtection();
+				}
+
 				// Trigger a custom event after the AJAX request is complete
 				$form.trigger('magic-login:login:ajax-success', [response]);
 			}
@@ -44,4 +51,51 @@ window.magicLoginAjaxEnabled = true;
 			$form.trigger('magic-login:login:always');
 		});
 	});
+
+	$(document).on('submit', '#magiclogincodeform', function (e) {
+		e.preventDefault();
+
+		const $form = $(this);
+
+		$form.trigger('magic-login:code-login:before-submit');
+
+		$.ajax({
+			type      : 'POST',
+			url       : $form.data('ajax-url'),
+			data      : $form.serialize() + '&action=magic_login_code_login',
+			beforeSend: function () {
+				const spinnerImg = '<img src="' + $form.data('ajax-spinner') + '" alt="' + $form.data('ajax-sending-msg') + '" class="magic-login-spinner-image" />';
+				const spinnerMessage = '<span class="magic-login-spinner-message">' + $form.data('ajax-sending-msg') + '</span>';
+				const spinnerHtml = '<div class="magic-login-spinner-container">' + spinnerImg + spinnerMessage + '</div>';
+				$('.magic-login-code-form-header').find('.spinner').html(spinnerHtml).show();
+				$form.find('.magic-login-code-submit').attr('disabled', 'disabled');
+				$form.trigger('magic-login:code-login:before-send');
+				$('.magic-login-code-form-header').find('.ajax-result').html('').removeClass('success error').hide();
+			},
+			success   : function (response) {
+				if(response.success && response.data.message) {
+					$('.magic-login-code-form-header').find('.ajax-result').html(response.data.message).addClass('success').removeClass('error').show();
+				}
+
+				if (response.data.redirect_to) {
+					window.location.href = response.data.redirect_to;
+				}
+
+				if (!response.success) {
+					$('.magic-login-code-form-header').find('.ajax-result').html(response.data.message).addClass('error').removeClass('success').show();
+				}
+
+				$form.trigger('magic-login:code-login:ajax-success', [response]);
+			}
+		})
+			.fail(function (response) {
+				$form.trigger('magic-login:code-login:ajax-fail', [response]);
+			})
+			.always(function () {
+				$form.find('.magic-login-code-submit').attr('disabled', false);
+				$form.trigger('magic-login:code-login:always');
+				$form.find('.spinner').hide();
+			});
+	});
+
 })(jQuery);
